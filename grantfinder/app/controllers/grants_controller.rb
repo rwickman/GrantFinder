435d32c77feb61aaf2d3c62656ee735d6443534a
Ryan Wickman <rwickman@memphis.edu>
2018-12-08 21:35:00 +0000

    Compute Cosine Values and produce search results

--- grantfinder/app/controllers/grants_controller.rb
+++ grantfinder/app/controllers/grants_controller.rb
@@ -1,9 +1,42 @@
+require 'socket'
+require 'json'
+
 class GrantsController < ApplicationController
     def index
-	@start = params['start'].to_i
-        @grant = Grant.all[@start, 15]
+	    @start = params['start'].to_i
+        #@grant = Grant.all[@start, 15]
+
+        # Create a TCP Socket to communicate with search engine (searchLSA.py)
+        host = Socket.gethostname
+        port = 50001
+        s = TCPSocket.new host, 50001
+        s.puts(params[:query])
+        json_results = ''
+        begin
+            while line = s.gets
+                json_results += line
+            end
+        rescue
+        end
+        s.close()
+        if !json_results.nil?
+            search_results = JSON.parse(json_results)
+            grants = Array.new
+            search_results.each do |result|
+                grants.push(Grant.find(result[0])) # Retrieve the grant from the database 
+            end
+            # puts grants
+        end
+
+        @grant = grants # These are the grant results from the search
     end
+
     def show
         @grant = Grant.find(params[:id])
     end
+    # def search
+    #     @start = params['start'].to_i
+    #     @grant = Grant.all[@start, 15]
+    #     puts(params[:query])
+    # end
 end
--- grantfinder/config/routes.rb
+++ grantfinder/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'home', to: 'pages#home', as: 'home'
   get 'results', to: 'grants#index', as:'results'
+  # get 'results/:search', to: 'grants#search', as: 'results_search'
   get 'results/:id', to: 'grants#show', as: 'result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
