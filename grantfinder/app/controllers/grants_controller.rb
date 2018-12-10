650f9359ae6d174415dd4c735fac97a9d6bdb5af
Chris Odum <rcjo52@gmail.com>
2018-12-10 21:00:08 +0000

    Added back button to show page

--- grantfinder/app/controllers/grants_controller.rb
+++ grantfinder/app/controllers/grants_controller.rb
@@ -1,9 +1,11 @@
 class GrantsController < ApplicationController
+    
     def index
 	@start = params['start'].to_i
         @grant = Grant.all[@start, 15]
     end
     def show
         @grant = Grant.find(params[:id])
+
     end
 end
