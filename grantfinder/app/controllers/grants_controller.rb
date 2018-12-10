3b0410753dce79d163c224621b2ca4750447a605
Chris Odum <rcjo52@gmail.com>
2018-12-10 19:20:17 +0000

    Added user seed data and added requirement that user be logged in to use app

--- grantfinder/app/controllers/grants_controller.rb
+++ grantfinder/app/controllers/grants_controller.rb
@@ -1,4 +1,5 @@
 class GrantsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index]
     def index
 	@start = params['start'].to_i
         @grant = Grant.all[@start, 15]
